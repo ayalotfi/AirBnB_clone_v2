e42f67e9cd7f68f698c4eb54ae7ba8c54eacd4a3
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-09-20 18:03:43 +0200

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,27 @@ Contains class BaseModel
 
 from datetime import datetime
 import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
+
 
 class BaseModel:
     """The BaseModel class from which future classes will be derived"""
+
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
